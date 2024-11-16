740a7a1494c391c6f40553deeb4d657947a4bfaf
ArseniyChepygin <arseniych735@gmail.com>
2024-11-16 19:03:51 +0100

    added UTC time

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-function updateClock(){
+function updateClock(time){
     const now = new Date();
-    const hours =  String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const hours =  String(now.getUTCHours() + time).padStart(2, '0');
+    const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(now.getUTCSeconds()).padStart(2, '0');
 
     document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+
+    
+    const selecttimezone = document.getElementById("timezone");
+    timezone2 = parseInt(selecttimezone.value, 10);
+    console.log(timezone2);
+
+    selecttimezone.addEventListener('change', updateClock(timezone2))
 }
 
-setInterval(updateClock, 1000);
+
+let timezone2 = 0;
+
+setInterval(updateClock(timezone2), 1000);
+updateClock(timezone2);
+
+
+
+const difference_time_UTC = new Date().getTimezoneOffset() / 60;
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+document.getElementById('clock2').textContent = timezone;
 
