59d5bacbddba9c5295853b6a3eaaf5115f3b51b2
ArseniyChepygin <arseniych735@gmail.com>
2024-11-16 18:00:18 +0100

    print clock (local time) in s

--- script.js
+++ script.js
@@ -9,14 +9,3 @@ function updateClock(){
 
 setInterval(updateClock, 1000);
 
-function updateClock_timezone(){
-    const now = new Date();
-    const selectedTimezone = -5; //New York
-    const hours =  String(now.getHours()-1+selectedTimezone).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-
-    document.getElementById('timezone').textContent = `${hours}:${minutes}:${seconds}`;
-}
-
-setInterval(updateClock_timezone, 1000);
